5201517831b0a282f3c65724f0327c2bd041f076
jnnkB <29376715+jnnkB@users.noreply.github.com>
2018-06-16 15:34:28 +0200

    added Unity Project

---
+++ Unity Project/Assets/ControlCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ControlCamera : MonoBehaviour {
+	public GameObject Cube;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Cube.transform.position;
+		transform.rotation = Cube.transform.rotation;
+	}
+}
---
+++ Unity Project/Assets/ControlCube.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ControlCube : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("w")) {
+			transform.Translate(Vector3.forward * Time.deltaTime);
+		}
+		if (Input.GetKey("s")) {
+			transform.Translate(Vector3.back * Time.deltaTime);
+		}
+		if (Input.GetKey("d")) {
+			transform.Rotate(0.0f,1f,0.0f);
+		}
+		if (Input.GetKey("a")) {
+			transform.Rotate(0.0f,-1f,0.0f);
+		}
+		
+		
+	}
+}
